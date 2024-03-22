22e643dcd0b59343a216a65ef254b358203d8075
Kai Chevannes <chevannes.kai@gmail.com>
2024-03-22 19:08:45 +0000

    new scripts again again again

--- zscripts/script_generator.py
+++ zscripts/script_generator.py
@@ -1,16 +1,17 @@
 import os
 import numpy as np
 
-of_initial_values = [500] + list(np.linspace(5000, 13000, 10, dtype=int))
+# 0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0
+of_initial_values = [500] + list(np.linspace(5000, 13000, 20, dtype=int))
 of_end_values = (
     [500]
-    + list(np.linspace(13000, 500, 5, dtype=int))
-    + list(np.linspace(500, 500, 5, dtype=int))
+    + list(np.linspace(13000, 500, 10, dtype=int))
+    + list(np.linspace(500, 500, 10, dtype=int))
 )
 ofs = []
 
 for i in range(len(of_initial_values)):
-    if i <= 5:
+    if i <= 10:
         of = [500] + list(
             np.linspace(of_initial_values[i], of_end_values[i], 100, dtype=int)
         )
@@ -23,14 +24,15 @@ for i in range(len(of_initial_values)):
     ofs.append(of)
 
 for i in range(10):
+    # 0.1, 0.2, etc
     for j in range(101):
         if not os.path.isdir(f"of0p{i}"):
             os.mkdir(f"of0p{i}")
         with open(f"./of0p{i}/of0p{i}oe{j}.slurm", "w") as outfile:
             outfile.write("#!/bin/bash\n")
-            outfile.write("#SBATCH --ntasks=40\n")
+            outfile.write("#SBATCH --ntasks=10\n")
             outfile.write("#SBATCH --nodes=1\n")
-            outfile.write("#SBATCH --ntasks-per-node=40\n")
+            outfile.write("#SBATCH --ntasks-per-node=10\n")
             outfile.write("#SBATCH --cpus-per-task=1\n")
             outfile.write("#SBATCH --time=03:00:00\n")
             outfile.write("cd /lyceum/kc2g21/Dissertation\n")
@@ -38,7 +40,31 @@ for i in range(10):
             outfile.write("source activate myenv\n")
             outfile.write("module load openmpi/4.1.1/gcc\n")
             outfile.write(
-                f"mpirun -np 40 python main.py distancetogoal -of 0.{i} -oe {j} -sp {j} -p 300 -t {ofs[i][j]} -f of0p{i}oe0to100.json\n"
+                f"mpirun -np 10 python main.py distancetogoal -of 0.{i} -oe {j} -sp {j} -p 300 -t {ofs[2*i][j]} -f of0p{i}oe0to100.json\n"
+            )
+
+for i in range(10):
+    # 0.05, 0.15, 0.25, etc
+    new_i = i + 0.5
+    formatted_new_i = f"{int(new_i * 10):02}"
+    for j in range(101):
+        if not os.path.isdir(f"of0p{formatted_new_i}"):
+            os.mkdir(f"of0p{formatted_new_i}")
+        with open(
+            f"./of0p{formatted_new_i}/of0p{formatted_new_i}oe{j}.slurm", "w"
+        ) as outfile:
+            outfile.write("#!/bin/bash\n")
+            outfile.write("#SBATCH --ntasks=10\n")
+            outfile.write("#SBATCH --nodes=1\n")
+            outfile.write("#SBATCH --ntasks-per-node=10\n")
+            outfile.write("#SBATCH --cpus-per-task=1\n")
+            outfile.write("#SBATCH --time=03:00:00\n")
+            outfile.write("cd /lyceum/kc2g21/Dissertation\n")
+            outfile.write("module load conda\n")
+            outfile.write("source activate myenv\n")
+            outfile.write("module load openmpi/4.1.1/gcc\n")
+            outfile.write(
+                f"mpirun -np 10 python main.py distancetogoal -of 0.{formatted_new_i} -oe {j} -sp {j} -p 300 -t {ofs[(2 * i) + 1][j]} -f of0p{formatted_new_i}oe0to100.json\n"
             )
 
 for j in range(101):
@@ -46,9 +72,9 @@ for j in range(101):
         os.mkdir("of1p0")
     with open(f"./of1p0/of1p0oe{j}.slurm", "w") as outfile:
         outfile.write("#!/bin/bash\n")
-        outfile.write("#SBATCH --ntasks=40\n")
+        outfile.write("#SBATCH --ntasks=10\n")
         outfile.write("#SBATCH --nodes=1\n")
-        outfile.write("#SBATCH --ntasks-per-node=40\n")
+        outfile.write("#SBATCH --ntasks-per-node=10\n")
         outfile.write("#SBATCH --cpus-per-task=1\n")
         outfile.write("#SBATCH --time=03:00:00\n")
         outfile.write("cd /lyceum/kc2g21/Dissertation\n")
@@ -56,5 +82,5 @@ for j in range(101):
         outfile.write("source activate myenv\n")
         outfile.write("module load openmpi/4.1.1/gcc\n")
         outfile.write(
-            f"mpirun -np 40 python main.py distancetogoal -of 1.0 -oe {j} -sp {j} -p 300 -t {ofs[10][j]} -f of1p0oe0to100.json\n"
+            f"mpirun -np 10 python main.py distancetogoal -of 1.0 -oe {j} -sp {j} -p 300 -t {ofs[10][j]} -f of1p0oe0to100.json\n"
         )
