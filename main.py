97f0a832daa51c30d9c787b89dba31a3ab148da1
Kai Chevannes <chevannes.kai@gmail.com>
2024-04-18 11:59:28 +0100

    i am stupip, but not that stupid lets be reasonable here

--- main.py
+++ main.py
@@ -34,13 +34,13 @@ def main(args):
     # boid_swarm_adjuster.set_strategy(boid_swarm_adjuster.modify_n_from_edge)
 
     # Experiment 3:
-    boid_swarm_adjuster.set_k(5.5)
-    boid_swarm_adjuster.set_strategy(boid_swarm_adjuster.modify_n_plus_radius)
+    # boid_swarm_adjuster.set_k(5.5)
+    # boid_swarm_adjuster.set_strategy(boid_swarm_adjuster.modify_n_plus_radius)
 
     # Experiment 4:
-    # boid_swarm_adjuster.continuous = True
-    # boid_swarm_adjuster.set_k(5.5)
-    # boid_swarm_adjuster.set_strategy(boid_swarm_adjuster.modify_n_from_edge_plus_radius)
+    boid_swarm_adjuster.continuous = True
+    boid_swarm_adjuster.set_k(5.5)
+    boid_swarm_adjuster.set_strategy(boid_swarm_adjuster.modify_n_from_edge_plus_radius)
 
     # boid_swarm_adjuster.set_velocity_multiplier(1.1)
     # boid_swarm_adjuster.set_strategy(boid_swarm_adjuster.modify_n_plus_velocity)
